836fd937b97565becefaf70b27254d6860b879c8
cemilkb <cemilkoybasi@hotmail.com>
2023-10-04 10:18:41 +0300

    Sadece password match işlemi kaldı

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
 let inputs = document.querySelectorAll("input")
+let password = document.querySelector(`input[name="password"]`)
+let password2 = document.querySelector(`input[name="password2"]`)
 let sButton = document.querySelector("#s-button")
+let check = null
+let ok = 0
+console.log(password.value)
+
+
+
+
 // function
 
 function validation(e) {
+
+
     if (e.validity.valid === false) {
         e.style.borderBottom = "1px solid red"
         e.style.backgroundColor = " rgba(255, 0, 0, 0.3)"
         let span = document.querySelector(`span[id=${e["name"]}]`)
         span.style.display = "block"
-        console.log(span)
     } else {
         e.style.borderBottom = "1px solid green"
         e.style.backgroundColor = "rgba(4, 255, 0, 0.1)"
         let span = document.querySelector(`span[id=${e["name"]}]`)
         span.style.display = "none"
     }
+
+
+    if (e["name"] === "password") {
+        check = e.value
+    }
+
+    if (e["name"] === "password2") {
+        if (e.value !== check) {
+            e.style.borderBottom = "1px solid red"
+            e.style.backgroundColor = " rgba(255, 0, 0, 0.3)"
+            let span = document.querySelector(`span[id=${e["name"]}]`)
+            span.style.display = "block"
+        }
+    }
+
 }
 
 
@@ -28,10 +53,12 @@ inputs.forEach(e => {
     })
 });
 
+if (password.value === password2.value) {
+    password2.validity.valid = false
+}
+
 
 sButton.addEventListener("click", (e) => {
-    inputs.forEach(e => {
-        validation(e)
-    })
+
 })
 
